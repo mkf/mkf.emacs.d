6c8eae1db6bf95e1657eb02cb0055c8d9b7635be
Michał Krzysztof Feiler <archiet@wmi.amu.edu.pl>
2019-12-02 18:26:24 +0100

    wombat enabled

--- init.el
+++ init.el
@@ -21,9 +21,7 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (wombat)))
  '(package-selected-packages
    (quote
     (mmm-mako python-mode mmm-jinja2 magit emmet-mode which-key))))
